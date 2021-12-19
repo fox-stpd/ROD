1e56d35fa27d3cc89b4682ed0a47f05dc83051dc
Анохина Анастасия М20-ИСТ-3 <73996020+fox-stpd@users.noreply.github.com>
2021-12-19 19:30:11 +0300

    Update countryModals.js

--- client/src/components/modals/countryModals.js
+++ client/src/components/modals/countryModals.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Button, Form, Modal} from "react-bootstrap";
+import {Button, Form, Modal} from "react-bootstrap"; // eslint-disable-next-line
 import {createCountry, createType} from "../../http/productAPI";
 
 const CountryModals = ({show, onHide}) => {
