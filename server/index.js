8d1712300b58c962fedcd0b30011ecbeb08793d6
semtrofi <semen.ed.trofimov@megafon.ru>
2021-12-20 00:03:48 +0300

    make it better4

--- server/index.js
+++ server/index.js
@@ -9,14 +9,15 @@ const errorHandler = require('./meddleware/ErrorHandlingMaddeleware')
 const path = require('path')
 
 const PORT = process.env.PORT || 5000
-
-app.use(express.static(path.join(__dirname, 'client/build')));
+const app = express()
+// app.use(express.static(path.join(__dirname, 'client/build')));
+app.use('/', express.static(path.join(__dirname, 'client', 'build')));
 
 app.get('*', (req,res) =>{
     res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
-const app = express()
+
 app.use(cors())
 app.use(express.json())
 app.use(express.static(path.resolve(__dirname, 'static')))
