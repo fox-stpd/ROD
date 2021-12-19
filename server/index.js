a0e9ccf523687a8e44706efeae05e10d0525f08b
Анохина Анастасия М20-ИСТ-3 <73996020+fox-stpd@users.noreply.github.com>
2021-12-19 22:42:14 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,6 +12,10 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 const app = express()
 app.use(cors())
 app.use(express.json())
