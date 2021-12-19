93645f402ed8bba169ba5e5d12a615a24f6fce67
semtrofi <semen.ed.trofimov@megafon.ru>
2021-12-20 00:19:38 +0300

    make it better6

--- server/db.js
+++ server/db.js
@@ -1,5 +1,8 @@
 const {Sequelize} = require('sequelize')
 
+var pg = require('pg');
+pg.defaults.ssl = true;
+
 module.exports = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
@@ -7,6 +10,14 @@ module.exports = new Sequelize(
     {
         dialect: 'postgres',
         host: process.env.DB_HOST,
-        port: process.env.DB_PORT
+        port: process.env.DB_PORT,
+        ssl:true,
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
     }
+
 )
