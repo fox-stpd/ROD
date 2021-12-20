33f3bcb2077033f5fc28d1ef8593e43f44c7f160
semtrofi <semen.ed.trofimov@megafon.ru>
2021-12-20 15:08:23 +0300

    make it better9

--- server/db.js
+++ server/db.js
@@ -18,4 +18,11 @@ const {Sequelize} = require('sequelize')
 //         }
 //     }
 
-module.exports = new Sequelize(process.env.DATABASE_URL);
+module.exports = new Sequelize(process.env.DATABASE_URL,{
+    dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+});
