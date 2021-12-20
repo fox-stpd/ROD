2d5b913ff4fe2d86bc7418f8348f90acb7c8d3c3
semtrofi <semen.ed.trofimov@megafon.ru>
2021-12-20 08:43:21 +0300

    make it better25

--- server/index.js
+++ server/index.js
@@ -19,6 +19,12 @@ app.use(cors({
     origin: true
 }))
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/', express.static(path.join(__dirname,'..', 'client', 'build')));
 
 app.get('*', (req, res) =>{
